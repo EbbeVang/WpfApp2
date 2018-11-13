a19366abea26546ab0a367532e7ec930c94f44fe
laptop\Ebbe Vang <ev@evang.dk>
2018-11-13 10:01:09 +0100

    Initial commit

---
+++ WpfApp2/Elephant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    class Elephant
+    {
+        private String name;
+        private int _weight;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Weight
+        {
+            get { return _weight; }
+            set { _weight = value; }
+        }
+    }
+}
---
+++ WpfApp2/ViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace WpfApp2
+{
+    class ViewModel
+    {
+        private ObservableCollection<Elephant> _list = new ObservableCollection<Elephant>();
+        
+        public ViewModel()
+        {
+            List.Add(new Elephant() {Name = "Adam", Weight = 300});
+            List.Add(new Elephant() {Name = "Bob", Weight = 100});
+            List = new ObservableCollection<Elephant>(List.OrderBy(elephant => elephant.Name).ToList());
+
+
+        }
+
+        public ObservableCollection<Elephant> List
+        {
+            get { return _list; }
+            set { _list = value; }
+        }
+    }
+}
